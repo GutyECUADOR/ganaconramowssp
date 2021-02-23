04afec70ec6ca05819b97eb8c920d0ab85f533fe
José L. Gutiérrez <gutiecuador@gmail.com>
2021-02-22 22:23:21 -0500

    add new banner

--- views/modulos/sis_modules/promo_banner.php
+++ views/modulos/sis_modules/promo_banner.php
@@ -1,12 +1,9 @@
 
-<div style="background-color: #e9092b">
+<div class="container-fluid text-center" style="background-color: #e9092b">
   <a href="https://tiendaramo.com/" target="_blank">
-    <marquee style="color:white; margin-top:10px; background-color:#e9092b" direction="left" width="100%" height="100%" scrollamount="10">
-      <img src="assets/img/logoramo.png" alt="Promo" style="max-height:20px">
-      !Haz clic aqui y sigue acumulando puntos!
-      <img src="assets/img/logoramo.png" alt="Promo" style="max-height:20px">
-      
-    </marquee>
+   
+      <img src="assets/img/botontiendaramo.jpg" alt="Promo" style="max-height:40px">
+   
     
   </a>
   
