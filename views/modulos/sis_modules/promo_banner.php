3b6195448f1f3ccd350be4eb5f41a39ac93f5393
José L. Gutiérrez <gutiecuador@gmail.com>
2021-02-24 12:26:19 -0500

    fix banner fuera del margen

--- views/modulos/sis_modules/promo_banner.php
+++ views/modulos/sis_modules/promo_banner.php
@@ -2,7 +2,7 @@
 <div class="container-fluid text-center" style="background-color: #e9092b">
   <a href="https://tiendaramo.com/" target="_blank">
    
-      <img src="assets/img/botontiendaramo.jpg" alt="Promo" style="max-height:40px">
+      <img src="assets/img/botontiendaramo.jpg" alt="Promo" style="max-height:40px; max-width: 100%;">
    
     
   </a>
